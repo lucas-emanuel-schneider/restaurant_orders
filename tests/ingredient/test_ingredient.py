99fb9dd9e4e9affd81997fd5b0a0349a4d23ef18
Lucas <lucas_emanuel_sc@hotmail.com>
2023-05-16 10:58:41 -0300

    feat: req: 01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,18 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1.
 def test_ingredient():
-    pass
+    chicken_meat = Ingredient('frango')
+    cheese = Ingredient('queijo mussarela')
+    egg = Ingredient('ovo')
+    assert repr(chicken_meat) == "Ingredient('frango')"
+    assert hash(chicken_meat) == hash(Ingredient('frango'))
+    assert hash(cheese) != hash(egg)
+    assert egg.name == 'ovo'
+    assert chicken_meat.__eq__(Ingredient('frango')) is True
+    assert chicken_meat.__eq__(egg) is False
+    assert cheese.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED
+    }
